a06ede100d00a07568001e7a14eca7a3d6bf4ff7
yezy <yezileliilomo@hotmail.com>
2017-10-07 13:39:06 +0300

    Improving code readability by removing unnecessary codes blocks

--- database/raw_db.py
+++ database/raw_db.py
@@ -2,15 +2,15 @@ from dorm import db;
 import config
 
 class course(db.model):
-    name=db.field(type="char(20)",constrain="not null", key="primary")
+    name=db.field(type="char(20)", key="primary")
     description=db.field(type="text")
     college=db.field(type="char(20)",constrain="not null")
 
 class student(db.model):
-    reg_no=db.field(type="char(20)",constrain="not null",key="primary")
+    reg_no=db.field(type="char(20)",key="primary")
     full_name=db.field(type="char(50)",constrain="not null")
-    course=db.field(type="char(20)",constrain="not null", key="foreign", ref="course.name")
     year=db.field(type="int", constrain="not null")
+    course=db.field(type="char(20)",constrain="not null", key="foreign", ref="course.name")
 
 def run():
    course().create()
--- dorm_test.py
+++ dorm_test.py
@@ -1,7 +1,8 @@
+import os
+import random
+import string
+import collections
 from flask import Flask, render_template, request
-from werkzeug import secure_filename
-from flaskext.mysql import MySQL
-import os, random, string, importlib
 from dorm import db
 from database import config
 
@@ -21,66 +22,48 @@ def randstr(length):
    return st
 app.jinja_env.globals.update(get_rec_values=get_rec_values)
 
-
 @app.route('/',methods = ['POST', 'GET'])
 def home():
    max_data=500
    if request.method=="GET":
      rand_table=db.random_table()
-     table=getattr(db,rand_table)
-     data=table().get()
-
-     check=""
-     if isinstance(data,db.custom_tuple_write) or isinstance(data,db.custom_tuple_read):
-        check='tuple'
-     elif isinstance(data,str) or isinstance(data,int) or isinstance(data,float) or isinstance(data, tuple):
-        check='value'
-     elif data==None :
-        check='None'
-        data=globals()[splitted_query[0]].get()
-     else:
-        check=='object'
-        val={}
-        for cl in tb_col:
-           val.update({cl:get_rec_values(data,cl)})
-        data=val
+     get_table=getattr(db,rand_table)
+     table=get_table()
+     data=table.get()
      if len(data)>max_data:
          data=data[:max_data]
-     return render_template("home.html",sq=data, data=check, table_used=table().table__name__)
+     return render_template("home.html",data=data, flag='tuple', table_used=table.table__name__)
 
    if request.method=="POST":
      query=request.form['query']
      splitted_query=query.split('.')
      data=""
-
-     if splitted_query[1].startswith( "sql(" )  or  splitted_query[2].startswith( "join(" ) :
-         result=eval(query)
-         if isinstance(result, db.custom_tuple_write) or isinstance(result, db.custom_tuple_read):
-             tb_name=result[0].table__name__
-         else:
-             tb_name = "Intermediate"
-         data=result
+     tb_name="Unkown"
+     if splitted_query[1].startswith( "execute(" ):
+         data=eval(query)
+         check="value"
+         if data is None:
+            data="Query done!.."
+         else:  ########################This is for making execute() UI more friendly####
+            rand_table=db.random_table()
+            get_table=getattr(db,rand_table)
+            table=get_table()
+            table.table__name__=query
+            table.table__columns__=data.columns
+            table.primary__keys__=data.columns
+            result=db.get_objects(data.fetch, data.columns, table)
+            data=db.custom_tuple_read(result)
+            check="tuple"
+            #############################################################################
      else:
-        tb_name=getattr(globals()['db'], splitted_query[1][:len(splitted_query[1])-2])().table__name__
+        tb_name=eval("db."+splitted_query[1]).table__name__
         data=eval(query)
 
+        if isinstance(data,db.custom_tuple_read):
+            check="tuple"
+            if len(data)>max_data:
+               data=data[:max_data]
+        else:
+           check="value"
 
-     check=""
-     if isinstance(data, db.custom_tuple_write) or isinstance(data,db.custom_tuple_read):
-        check='tuple'
-     elif isinstance(data,str) or isinstance(data,int) or isinstance(data,float) or isinstance(data, tuple):
-        check='value'
-     elif data==None :
-        check='None'
-        command="db."+splitted_query[1]+".get()"
-        data=eval(command)
-     else:
-        check=='object'
-        val={}
-        for col in tb_col:
-           val.update({col:get_rec_values(data,col)})
-        data=val
-
-     if len(data)>max_data:
-         data=data[:max_data]
-     return  render_template("home.html",sq=data,qr=query, data=check, table_used=tb_name)
+     return  render_template("home.html",data=data,qr=query, flag=check, table_used=tb_name)
